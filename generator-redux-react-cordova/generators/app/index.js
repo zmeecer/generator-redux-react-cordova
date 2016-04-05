aede0918046025dd0f411eba4e3b11ccc7aeda3c
Źmicier Jaraševič <zmeecer@gmail.com>
2016-04-05 16:44:38 +0300

    updated prompting section

--- generator-redux-react-cordova/generators/app/index.js
+++ generator-redux-react-cordova/generators/app/index.js
@@ -3,26 +3,51 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 
+var CONST = {
+  ID: '_id',
+  NAME: '_name',
+  DESCRIPTION: '_desctiption',
+  VERSION: '_version',
+  AUTHOR: '_author',
+}
 module.exports = yeoman.generators.Base.extend({
   prompting: function () {
     var done = this.async();
 
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the super-duper ' + chalk.red('') + ' generator!'
+      'Welcome to the React + Redux + Cordova ' + chalk.red('') + ' generator!'
     ));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    var prompts = [
+      {
+        type: 'input',
+        name: CONST.ID,
+        message: 'Enter Cordova bundle ID of the project',
+        default: 'org.default.project'
+      },
+      {
+        type: 'input',
+        name: CONST.NAME,
+        message: 'Enter name of the projece (by npm rules)',
+        default: 'ProjectY'
+      },
+      {
+        type: 'input',
+        name: CONST.VERSION,
+        message: 'Enter version (SemVer notation)',
+        default: '0.1.0'
+      },
+      {
+        type: 'input',
+        name: CONST.AUTHOR,
+        message: 'Enter author name (personal or corporative)',
+        default: 'Author'
+      },
+    ];
 
     this.prompt(prompts, function (props) {
       this.props = props;
-      // To access props later use this.props.someOption;
-
       done();
     }.bind(this));
   },
